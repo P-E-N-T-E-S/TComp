8deaa64bb6332ccddd06285c1e66fcc96387680c
Thomazrlima <thomazlima211@gmail.com>
2024-11-25 13:20:39 -0300

    Trocando de Quick para merge

--- testes/worstmerge.py
+++ testes/worstmerge.py
@@ -3,34 +3,34 @@ import random
 import pandas as pd
 import os
 
-def quicksort(v, inicio, fim):
-    if inicio < fim:
-        p = particao(v, inicio, fim)
-        quicksort(v, inicio, p - 1)
-        quicksort(v, p + 1, fim)
+def merge_sort(lista):
+    if len(lista) > 1:
+        mid = len(lista) // 2
+        left = lista[:mid]
+        right = lista[mid:]
+        merge_sort(left)
+        merge_sort(right)
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                lista[k] = left[i]
+                i += 1
+            else:
+                lista[k] = right[j]
+                j += 1
+            k += 1
+        while i < len(left):
+            lista[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            lista[k] = right[j]
+            j += 1
+            k += 1
 
-def particao(v, inicio, fim):
-    pivot = v[inicio]
-    indice = fim
-
-    for i in range(fim, inicio, -1):
-        if v[i] <= pivot:
-            v[i], v[indice] = v[indice], v[i]
-            indice -= 1
-
-    v[inicio], v[indice] = v[indice], v[inicio]
-    return indice
-
-def kth_smallest_quick(v, k):
-    inicio, fim = 0, len(v) - 1
-    while inicio <= fim:
-        p = particao(v, inicio, fim)
-        if p == k:
-            return v[p]
-        elif p < k:
-            inicio = p + 1
-        else:
-            fim = p - 1
+def kth_smallest_merge(lista, k):
+    merge_sort(lista)
+    return lista[k - 1]
 
 def verificar_ordem(lista):
     if lista == sorted(lista):
@@ -48,15 +48,13 @@ def executar_experimentos(tamanhos_listas, num_repeticoes=10):
 
         for _ in range(num_repeticoes):
             random.seed()
-
-            lista = list(range(tamanho, 0, -1))
-
+            lista = random.sample(range(1, 10000), tamanho)
             k = tamanho // 2
 
             ordem_lista = verificar_ordem(lista)
 
             start_time = time.perf_counter()
-            kth_smallest_quick(lista, k)
+            kth_smallest_merge(lista, k)
             end_time = time.perf_counter()
 
             tempo_execucao = end_time - start_time
@@ -70,7 +68,7 @@ def executar_experimentos(tamanhos_listas, num_repeticoes=10):
             'Lista Ordenada': ordem_lista
         })
 
-    arquivo_saida = 'worst_experimentos_quick.csv'
+    arquivo_saida = 'resultados_experimentos_merge.csv'
     
     if os.path.isfile(arquivo_saida):
         df_resultados = pd.DataFrame(resultados)
